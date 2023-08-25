6f71402b96b906cf54a4ee0fd936b86e7b568cf8
root <am8122004@gmail.com>
2023-08-25 04:05:58 -0700

    fix check 1 of pop function

--- stack_functions1.c
+++ stack_functions1.c
@@ -111,7 +111,7 @@ void pop(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		cleanup();
 		free_stack(*stack);
 		exit(EXIT_FAILURE);
